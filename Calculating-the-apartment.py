d69c56369bb399638f25f0f835af158489ee1bb2
Mykyta <69142225+Romanennko@users.noreply.github.com>
2024-10-26 21:40:10 +0200

    Throwing over the old programm

---
+++ Calculating-the-apartment.py
@@ -0,0 +1,24 @@
+try:
+    input_apartments = int(input("Enter the quatriplegic: "))
+    passability = True
+    floors = 0
+    if input_apartments >= 430: passability = False
+
+    if passability == True and input_apartments <=287:
+        if input_apartments <= 11:
+            print(f"Apartment: {input_apartments}\nFloor: 2\nSection: 1")
+        else:
+            floors = input_apartments // 12
+            floors += 2
+            print(f"Apartment: {input_apartments}\nFloor: {floors}\nSection: 1")
+    if passability == True and input_apartments >=288:
+        if input_apartments <= 293:
+            print(f"Apartment: {input_apartments}\nFloor: 2\nSection: 2")
+        else:
+            floors = input_apartments // 6
+            floors -= 46
+            print(f"Apartment: {input_apartments}\nFloor: {floors}\nSection: 2")
+    if passability == False:
+        print("The most recent apartment is 430.")
+except:
+    print("You entered the wrong thing")
